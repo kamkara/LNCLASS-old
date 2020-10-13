f44f4c7d05828825c79adde1d3ad2bdf8583b494
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-10-13 10:00:15 +0000

    install bootstrap 5alpha

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import "../css/application.css"
+
+//= require jquery3
+//= require popper
+//= require bootstrap
