6687f649793a7042e2c2fcc6c3aa89371d47fb0c
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-10-14 08:59:54 +0000

    main consolide

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+
+
   def index
+    if current_user.role == 'student'
+      @courses = Course.where("level= ?", current_user.level)#.order("created_at")
+    elsif current_user.role == "prof"
+      @courses = Course.all
+    end
   end
 end
