82acd1c5e1b4a02522812ca322c60dd8b97c9d32
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-10-14 11:35:07 +0000

    build home feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+
+  before_action :authenticate_user!
   def index
+    if current_user.role == "student"
+      @courses = Course.where("level= ?", current_user.level)
+    else current_user.role == "teacher"
+      @courses = Course.all
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :courses
-  root to:'courses#index'
+  root to:'home#index'
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
