210dfc3b004e1bbdc6c8424b5768d8fb9231cef8
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-10-14 09:54:49 +0000

    add custom onglet msg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  #custom onglet Msg
+  def title
+    base_title = "LNCLASS"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} - #{@title}"
+    end
+
+  end
 end
